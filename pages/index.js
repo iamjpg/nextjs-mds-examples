490ba1525ac6cc1427c9b33659f76aca0dd15c4e
J.P. Given <jgiven@gmail.com>
2022-12-28 12:09:27 -0800

    Some minor layout changes.

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,16 @@ export default function Home() {
   return (
     <>
       <ul>
-        <li>
+        <li className='my-10'>
           <Link href='/auto-complete'>Auto Complete</Link>
         </li>
-        <li>
+        <li className='my-10'>
           <Link href='/nested-table'>Table (nested example)</Link>
         </li>
-        <li>
+        <li className='my-10'>
           <Link href='/dropdowns'>Dropdowns</Link>
         </li>
-        <li>
+        <li className='my-10'>
           <Link href='/tab-content'>Tab Content</Link>
         </li>
       </ul>
