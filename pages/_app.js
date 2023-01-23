d7b75ab43cfe1286d60432e683a5aa355e6dda52
J.P. Given <jgiven@gmail.com>
2023-01-23 08:51:08 -0800

    Working on figuring out how these font icons work...

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import Layout from '../components/layout';
 
+// import '@progress/kendo-svg-icons';
 import '@moxiworks/mds-alpha/dist/styles/mds-core.css';
 import '../styles/globals.css';
-import '../styles/kendo-ui.css';
+// import '../styles/kendo-ui.css';
 import '../styles/kendo-overrides.css';
 
 export default function MyApp({ Component, pageProps }) {
